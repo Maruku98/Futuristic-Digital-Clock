05a7812e6845c0ab76f2e16de79a80d1f48b8429
Marc Parrilla <133391272+Maruku98@users.noreply.github.com>
2023-06-07 18:25:07 +0200

    Add files via upload

---
+++ Futuristic Digital Clock/scripts-clock.js
@@ -0,0 +1,30 @@
+//GET NUMBERS AND THE BUTTON AND STORE THEM INTO VARIABLES
+let clockNumbers = document.querySelectorAll(".clock-integers");
+let [hourHours, hourMinutes, hourSeconds] = clockNumbers;
+let clockButton = document.getElementById("button");
+let clock = document.querySelector("#clock");
+
+
+
+clockButton.addEventListener("click", futuristicClock);
+
+function futuristicClock() { 
+  clockButton.innerText = "CLOCK ON";
+
+  clockNumbers.forEach((element) => {  //STYLE CHANGES
+    element.style.color = "#FA0060";
+  });
+  clock.style.borderColor = "#FA0060";
+
+  let date = new Date();        //GET CURRENT HOURS, MINUTES, AND SECONDS
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  //SET INITIAL TIME BASED ON CURRENT TIME
+  hourHours.innerHTML = hours.toString().padStart(2, "0");
+  hourMinutes.innerHTML = minutes.toString().padStart(2, "0");
+  hourSeconds.innerHTML = seconds.toString().padStart(2, "0");
+
+  setTimeout(futuristicClock, 1000);
+};
